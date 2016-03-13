c50e6f75fbd0663a6c7e729e3427d20d64512634
Issaret Prachitmutita <imac.monochrome@gmail.com>
2016-03-13 22:51:07 +0700

    init all of function

---
+++ merge/plusMerge.go
@@ -0,0 +1,27 @@
+package plusMerge
+
+import ()
+
+func main() {
+
+}
+
+func isFileExist(path string) bool {
+	return true
+}
+
+func isFolderExist(path string) bool {
+	return true
+}
+
+func createFolder(path string) bool {
+	return true
+}
+
+func moveFile(pathDest string, pathSrc ...string) bool {
+	return true
+}
+
+func getChild(path string) bool {
+	return true
+}
---
+++ merge/plusMerge_test.go
@@ -0,0 +1,30 @@
+package plusMerge
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFileisExist(t *testing.T) {
+	var path = ""
+	assert.True(t, isFileExist(path))
+}
+
+func TestFolderisExist(t *testing.T) {
+	var path = ""
+	assert.True(t, isFolderExist(path))
+}
+
+func TestFolderCreateFolder(t *testing.T) {
+	var path = ""
+	assert.True(t, createFolder(path))
+}
+
+func TestMoveFile(t *testing.T) {
+	var pathA = ""
+	var pathB = ""
+	var pathC = ""
+
+	assert.True(t, moveFile(pathA, pathC))
+	assert.True(t, moveFile(pathA, pathB, pathC))
+}
